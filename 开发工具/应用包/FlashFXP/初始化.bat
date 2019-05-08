38e2e95838cd85bf201f8083d22aef5b59272ded
无影 <44466079+wuYingZunZhe@users.noreply.github.com>
2019-05-08 13:23:22 +0800

    Add files via upload

---
+++ 开发工具/应用包/FlashFXP/初始化.bat
@@ -0,0 +1,9 @@
+@echo off
+mode con cols=50 lines=20
+color 2F
+title FlashFXP4 官方简体绿色版
+echo 快乐无极 WWW.OYKSOFT.COM
+echo FlashFXP4 官方简体绿色版初始化
+Reg delete HKEY_LOCAL_MACHINE\SOFTWARE\FlashFXP\4 /f
+echo 初始化完成！
+pause
